0ac320f03daa16ff6a88ac5a03819602ffb8cd71
Marco Scapin <markoscapin@gmail.com>
2022-02-09 14:05:21 +0100

    v-1

--- printable.js
+++ printable.js
@@ -13,9 +13,9 @@ function createPrintable() {
   printable.innerHTML = `
     <div class="d-flex flex-row justify-content-center mx-4">
     <div class="d-flex flex-column">
-        <div class="text-center align-items-center text-wrap mb-5">
-             <img width=200 height=200 src="./logomobileoutfitters.svg">
-        <h3>Conserva la pellicola originale e attiva il servizio di "Garanzia a Vita"</h3>
+        <div class="text-center align-items-center text-wrap mb-3">
+             <img width=100 src="./logomobileoutfitters.svg">
+        <h3 class="mt-2">Conserva la pellicola originale e attiva il servizio di "Garanzia a Vita"</h3>
         </div>
         <h3>DETTAGLIO PRESTAZIONE</h3>
         <div class="callout callout-info">
@@ -26,28 +26,45 @@ function createPrintable() {
             <p>Punto Vendita: Savignano Sul Rubicone</p>
         </div>
         <!-- HERE WE NEED TO PLACE ALL PRODUCTS-->
-        <div class="callout callout-warning">
-            <p>Copertura Protetta: Front</p>
-            <p>Prodotto: Clear-Coat Original</p>
-            <p class="fw-bolder">Codice di Attivazione: ASD3-1234 </p>
+        <div class="callout callout-warning d-flex justify-content-between">
+            <div>
+                <p>Copertura Protetta: Front</p>
+                <p>Prodotto: Clear-Coat Original</p>
+                <p class="fw-bolder">Codice di Attivazione: ASD3-1234 </p>
+            </div>
+            <div class="my-auto border border-dark rounded p-1" id="qrcode-0"></div>
+        </div>
+        <div class="callout callout-warning d-flex justify-content-between">
+            <div>
+                <p>Copertura Protetta: Front</p>
+                <p>Prodotto: Clear-Coat Original</p>
+                <p class="fw-bolder">Codice di Attivazione: ASD3-1234 </p>
+            </div>
+            <div class="my-auto border border-dark rounded p-1" id="qrcode-1"></div>
+        </div>
+        <div class="d-flex flex-row flex-wrap">
+            <div>
+                <h3>RICEZIONE</h3>
+                <p class="desc-p">E' vietata l'installazione su dispositivi che presentino il display danneggiato. L'operatore dichiara di aver <b>ricevuto</b> dal cliente un dispositivo regolarmente funzionante e in buono stato di conservazione</p>
+                <div class="d-flex flex-column">
+                    <b><em>Firma Operatore</em></b>
+                    <hr style="border: solid 1px black" class="my-3"/>
+                    <b><em>Firma Cliente</em></b>
+                    <hr style="border: solid 1px black" class="my-3"/>
+                </div>
+            </div>
+            <div>
+                <h3>RESTITUZIONE</h3>
+                <p class="desc-p">Il Cliente dichiara che il prodotto è stato <b>restituito</b> dall'operatore regolarmente funzionante e in buono stato di conservazione, non rinvenendo alcun danno al dispositivo.</p>
+                <div class="d-flex flex-column">
+                    <b ><em>Firma Cliente</em></b>
+                    <hr style="border: solid 1px black" class="my-3"/>
+                    <b><em>Firma Operatore</em></b>
+                    <hr style="border: solid 1px black" class="my-3"/>
+                </div>
+            </div>
         </div>
         
-        <h3>RICEZIONE</h3>
-        <p>E' vietata l'installazione su dispositivi che presentino il display danneggiato. L'operatore dichiara di aver <b>ricevuto</b> dal cliente un dispositivo regolarmente funzionante e in buono stato di conservazione</p>
-        <div class="d-flex flex-column">
-            <b><em>Firma Operatore</em></b>
-            <hr style="border: solid 1px black" class="my-5"/>
-            <b><em>Firma Cliente</em></b>
-            <hr style="border: solid 1px black" class="my-5"/>
-        </div>
-        <h3>RESTITUZIONE</h3>
-        <p>Il Cliente dichiara che il prodotto è stato <b>restituito</b> dall'operatore regolarmente funzionante e in buono stato di conservazione, non rinvenendo alcun danno al dispositivo.</p>
-        <div class="d-flex flex-column">
-            <b ><em>Firma Cliente</em></b>
-            <hr style="border: solid 1px black" class="my-5"/>
-            <b><em>Firma Operatore</em></b>
-            <hr style="border: solid 1px black" class="my-5"/>
-        </div>
     </div>
 </div>
 
@@ -58,11 +75,11 @@ function createPrintable() {
   //   document.body.append(printable);
 
   //this is for the test
-  document.body.innerHTML = "";
   document.body.append(printable);
+  for (let i = 0; i < 2; i++) {
+    new QRCode(document.getElementById("qrcode-" + i), url + i);
+  }
   setTimeout(() => {
     window.print();
   }, 200);
-
-  //new QRCode(document.getElementById("qrcode"), url + deviceID);
 }
