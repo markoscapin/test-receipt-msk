c2d776fa93fe93df18aa971bbe330e4934b86456
Marco Scapin <markoscapin@gmail.com>
2022-02-09 17:54:40 +0100

    qr-code + margini

--- printable.js
+++ printable.js
@@ -17,7 +17,7 @@ function createPrintable() {
              <img width=100 src="./logomobileoutfitters.svg">
         <h3 class="mt-2">Conserva la pellicola originale e attiva il servizio di "Garanzia a Vita"</h3>
         </div>
-        <h3>DETTAGLIO PRESTAZIONE</h3>
+        <h3 class="text-center">DETTAGLIO PRESTAZIONE</h3>
         <div class="callout callout-info">
             <p>Nome Cliente: Mario Rossi</p>
             <p>Data di Acquisto: 08/02/2022</p>
@@ -43,7 +43,7 @@ function createPrintable() {
             <div class="my-auto" id="qrcode-1"></div>
         </div>
         <div class="d-flex flex-row flex-wrap">
-            <div>
+            <div  class="text-center px-2">
                 <h3>RICEZIONE</h3>
                 <p class="desc-p">E' vietata l'installazione su dispositivi che presentino il display danneggiato. L'operatore dichiara di aver <b>ricevuto</b> dal cliente un dispositivo regolarmente funzionante e in buono stato di conservazione</p>
                 <div class="d-flex flex-column">
@@ -53,11 +53,11 @@ function createPrintable() {
                     <hr style="border: solid 1px black" class="my-3"/>
                 </div>
             </div>
-            <div>
+            <div class="text-center px-3">
                 <h3>RESTITUZIONE</h3>
                 <p class="desc-p">Il Cliente dichiara che il prodotto è stato <b>restituito</b> dall'operatore regolarmente funzionante e in buono stato di conservazione, non rinvenendo alcun danno al dispositivo.</p>
                 <div class="d-flex flex-column">
-                    <b ><em>Firma Cliente</em></b>
+                    <b><em>Firma Cliente</em></b>
                     <hr style="border: solid 1px black" class="my-3"/>
                     <b><em>Firma Operatore</em></b>
                     <hr style="border: solid 1px black" class="my-3"/>
